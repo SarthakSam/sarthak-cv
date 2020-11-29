d30b0ede4e6b13238c07cb753047f7883b92bf55
Sarthak <sarthak242@gmail.com>
2020-11-29 13:50:36 +0530

    feat: added navigation between pages

---
+++ index.js
@@ -0,0 +1,16 @@
+window.addEventListener('DOMContentLoaded', () => {
+    let hash = document.location.hash;
+    changePage(hash.substring(1));
+}) 
+
+function changePage(hash) {
+    let pages = document.getElementsByClassName('page');
+    let pagesArr = Array.prototype.slice.call( pages );
+    pagesArr.forEach(page => {
+        // page.style.display = "none";
+        page.classList.add("displayNone");
+    });
+    let node = document.querySelector(`.${hash}`)
+    node.classList.remove("displayNone")
+    // node.style.display = "initial";
+}
